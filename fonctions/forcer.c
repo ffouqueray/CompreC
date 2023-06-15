6800bf36b73245302c07780ca8581c3ccd06700f
Flavien FOUQUERAY <65244389+ffouqueray@users.noreply.github.com>
2023-06-15 14:45:56 +0200

    Added multithreading

--- fonctions/forcer.c
+++ fonctions/forcer.c
@@ -2,8 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <zip.h>
+#include <pthread.h>
+#include <math.h>
 
 #include "navigation.h"
+
+// On fait des variables globales au fichier pour nos mots de passe
+pthread_mutex_t password_mutex = PTHREAD_MUTEX_INITIALIZER;
+char *current_password = NULL;
+char *charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int charset_len;
+int current_password_length = 0;
+int current_password_index = 0;
+
+// On fait notre propre structure de donnee pour les threads
+typedef struct {
+    const char *archive;
+    char *charset;
+} thread_data_t;
 
 // Code qui sert a l'ouverture avec un mot de passe
 struct zip* open_zip_with_password(const char *archive, const char *password) {
@@ -19,7 +35,7 @@ struct zip* open_zip_with_password(const char *archive, const char *password) {
     // On recupere tous les fichiers presents dans l'archive et on essaie de les dechiffrer avec le mdp qu'on a donne
     int num_files = zip_get_num_entries(za, 0);
     if (verbose == 1) {
-        printf("Trying password '%s'\n", guess);
+        printf("Trying password '%s'\n", password);
     }
     for(int i = 0; i < num_files; i++) {
         const char *name = zip_get_name(za, i, 0);
@@ -46,47 +62,103 @@ struct zip* open_zip_with_password(const char *archive, const char *password) {
         zip_close(za);
         return NULL;
     }
-
     // On sauvegarde le mot de passe dans une variable globale
     global_pwd = strdup(password);
 
     return za;
 }
 
-// Code qui sert a faire notre attaque de brute force sur X caracteres, et renvoie le mdp une fois trouve. Sinon elle renvoie NULL
-char* brute_force_attack(const char *archive, int max_length) {
-    struct zip *za;
-    const char *charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int charset_len = strlen(charset);
+// Fonction dediee à la generation de nouveaux mots de passe
+char *next_password() {
+
+    // On verrouille le mutex pour éviter que plusieurs threads ne modifient le mot de passe courant en même temps
+    pthread_mutex_lock(&password_mutex);
     
-    // Une boucle pour les caractères de 1 a max defini
-    for (int length = 1; length <= max_length; length++) {
-        char *guess = (char *)malloc((length + 1) * sizeof(char));
-        memset(guess, charset[0], length);
-        guess[length] = '\0';
+    // Si c'est le premier mot de passe a generer
+    if (current_password == NULL) {
+        // On alloue la memoire et initialisons toutes les variables
+        current_password = malloc(2);
+        current_password[0] = charset[0];
+        current_password[1] = '\0';
+        current_password_length = 1;
+        current_password_index = 0;
+    } else {
+        // On incremente l'index
+        current_password_index++;
 
-        do {
-            za = open_zip_with_password(archive, guess);
-            if (za != NULL) {
-                return guess;  // Password is correct, return it.
-            }
+        // Si on a fait le tour de la liste sur le nombre de combinaisons de cette longueur
+        if (current_password_index >= pow(charset_len, current_password_length)) {
+            // On alloue la memoire et reinitialisons l'index
+            current_password_length++;
+            current_password_index = 0;
+            current_password = realloc(current_password, current_password_length + 1);
+        }
 
-            // Generate next password
-            int index = length - 1;
-            while (index >= 0) {
-                char* pos = strchr(charset, guess[index]);
-                if (pos < charset + charset_len - 1) {  // If not at the last char in charset
-                    guess[index] = *(pos + 1);  // Move to the next char in charset
-                    break;
-                } else {
-                    guess[index] = charset[0];  // Reset this index to the first char in charset
-                    index--;
-                }
-            }
-        } while (guess[0] != charset[charset_len - 1] || guess[length - 1] != charset[charset_len - 1]);
+        // On génère le nouveau mot de passe en utilisant l'index du mot de passe et l'ensemble de caractères
+        for (int i = 0; i < current_password_length; i++) {
+            int index = (current_password_index / (int)pow(charset_len, i)) % charset_len;
+            current_password[i] = charset[index];
+        }
 
-        free(guess);
+        // On ajoute le caractère nul de fin de chaîne
+        current_password[current_password_length] = '\0';
     }
 
-    return NULL;  // No password was found within the max_length
+    // On cree une copie et on deverrouille le mutex
+    char *password = strdup(current_password);
+    pthread_mutex_unlock(&password_mutex);
+
+    return password;
+}
+
+// Fonction executee par chaque thread
+void *brute_force_thread(void *arg) {
+    thread_data_t *data = (thread_data_t *)arg;
+    char *password;
+
+    while ((password = next_password()) != NULL) {
+        open_zip_with_password(data->archive, password);
+
+        free(password);
+
+        // On check si un autre thread a trouve le mot de passe
+        if (global_pwd != NULL) {
+            pthread_exit(NULL);
+        }
+    }
+
+    return NULL;
+}
+
+// Fonction principale pour lancer l'attaque de brute force
+char* brute_force_attack(const char *archive, int num_threads) {
+    pthread_t threads[num_threads];
+    thread_data_t thread_data[num_threads];
+
+    charset_len = strlen(charset);
+
+    for (int i = 0; i < num_threads; i++) {
+        thread_data[i].archive = archive;
+        pthread_create(&threads[i], NULL, brute_force_thread, &thread_data[i]);
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+
+        // On check si un thread a trouve le mot de passe
+        if (global_pwd != NULL) {
+            // On annule tous les autres threads
+            for(int j = 0; j < num_threads; j++) {
+                if(i != j) {
+                    pthread_cancel(threads[j]);
+                }
+            }
+
+            free(current_password);
+            return global_pwd;
+        }
+    }
+
+    free(current_password);
+    return NULL;
 }
